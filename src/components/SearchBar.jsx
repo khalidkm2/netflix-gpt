e5cbdf57789cc19a19ad7adc177e249f64a114f9
khaidprogramming <khalidmiya399@gmail.com>
2024-03-25 21:55:48 +0545

    gpt search fixed

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,7 +2,7 @@ import React, { useRef } from 'react'
 import { languages } from '../utils/languages'
 import { useDispatch, useSelector } from 'react-redux'
 import { openai } from '../utils/openAi'
-import { API_KEY_OPTIONS } from '../utils/constants'
+import { API_KEY_OPTIONS, OPENAI_KEY } from '../utils/constants'
 import { addGptMovies } from '../utils/gptSlice'
 
 const SearchBar = () => {
@@ -24,15 +24,38 @@ const SearchBar = () => {
   
 
   const getDataGpt = async()=> {
-    const query = `Generate movie suggestions for${text.current.value} , separated by commas as like example: Thor,Thugs of Hindustan,Hera Pheri,Man Of Steel,Khalnayak`
-    const completion = await openai.chat.completions.create({
-      messages: [{ role: "system", content: query }],
-      model: "gpt-3.5-turbo",
-    });
+    const query = `Generate movie suggestions for${text.current.value} , separated by commas as like example: Thor,Thugs of Hindustan,Hera Pheri,Man Of Steel,Khalnayak. don't write extra word like here are some and don't write /n`
+    // const completion = await openai.chat.completions.create({
+    //   messages: [{ role: "system", content: query }],
+    //   model: "gpt-3.5-turbo",
+    // });
   
-    // console.log(completion?.choices[0]);
-    const gptMovies = completion?.choices[0]?.message?.content
+    // // console.log(completion?.choices[0]);
+    // const gptMovies = completion?.choices[0]?.message?.content
     // console.log(gptMovies);
+    const data = await fetch("https://corsproxy.io/?https://api.anthropic.com/v1/messages", {
+    method: "POST",
+    headers: {
+      "x-api-key":
+        OPENAI_KEY,
+      "anthropic-version": "2023-06-01",
+      "content-type": "application/json",
+    },
+    // body: '{\n    "model": "claude-3-opus-20240229",\n    "max_tokens": 1024,\n    "messages": [\n        {"role": "user", "content": "Hello, world"}\n    ]\n}',
+    body: JSON.stringify({
+      model: "claude-3-opus-20240229",
+      max_tokens: 1024,
+      messages: [
+        {
+          role: "user",
+          content: query,
+        },
+      ],
+    }),
+  });
+
+  const json = await data.json();
+  const gptMovies = json?.content[0]?.text;
     const gptMoviesList = gptMovies.split(',')
     // console.log(gptMoviesList);
     const allPromises = Promise.all(gptMoviesList.map((movie) => searchMovies(movie)))
