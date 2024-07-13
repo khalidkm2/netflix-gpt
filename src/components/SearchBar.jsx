b8ea3829296fe49549267b7585c543fd8b755099
khaidprogramming <khalidmiya399@gmail.com>
2024-07-13 10:13:58 +0545

    error handled

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,15 +1,21 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { languages } from '../utils/languages'
 import { useDispatch, useSelector } from 'react-redux'
 import { openai } from '../utils/openAi'
 import { API_KEY_OPTIONS, OPENAI_KEY } from '../utils/constants'
 import { addGptMovies } from '../utils/gptSlice'
+import { ColorRing } from 'react-loader-spinner'
+
+
 
 const SearchBar = () => {
   const text = useRef(null)
   const dispatch = useDispatch()
+  const[loader,setLoader] = useState(false)
 
   const lang = useSelector((store)=> store.config.lang)
+  const {gptMovies,gptResults} = useSelector((store) => store.gpt)
+
 
   const searchMovies = async(movieName) => {
     // console.log("insi");
@@ -34,48 +40,69 @@ const SearchBar = () => {
     // const gptMovies = completion?.choices[0]?.message?.content
     // console.log(gptMovies);
     // https://corsproxy.io/?
-    const data = await fetch("https://corsproxy.io/?https://api.anthropic.com/v1/messages", {
-    method: "POST",
-    headers: {
-      "x-api-key":
-        OPENAI_KEY,
-      "anthropic-version": "2023-06-01",
-      "content-type": "application/json",
-    },
-    // body: '{\n    "model": "claude-3-opus-20240229",\n    "max_tokens": 1024,\n    "messages": [\n        {"role": "user", "content": "Hello, world"}\n    ]\n}',
-    body: JSON.stringify({
-      model: "claude-3-opus-20240229",
-      max_tokens: 1024,
-      messages: [
-        {
-          role: "user",
-          content: query,
-        },
-      ],
-    }),
-  });
-
-  const json = await data.json();
-  const gptMovies = json?.content[0]?.text;
-    const gptMoviesList = gptMovies.split(',')
-    // console.log(gptMoviesList);
-    const allPromises = Promise.all(gptMoviesList.map((movie) => searchMovies(movie)))
-    
-    allPromises.then((results) => dispatch(addGptMovies({gptMoviesData:results, gptResults:gptMoviesList})))
+   
     // console.log("haha",allPromises);
+    try {
+      const data = await fetch("https://corsproxy.io/?https://api.anthropic.com/v1/messages", {
+        method: "POST",
+        headers: {
+          "x-api-key":
+            OPENAI_KEY,
+          "anthropic-version": "2023-06-01",
+          "content-type": "application/json",
+        },
+        // body: '{\n    "model": "claude-3-opus-20240229",\n    "max_tokens": 1024,\n    "messages": [\n        {"role": "user", "content": "Hello, world"}\n    ]\n}',
+        body: JSON.stringify({
+          model: "claude-3-opus-20240229",
+          max_tokens: 1024,
+          messages: [
+            {
+              role: "user",
+              content: query,
+            },
+          ],
+        }),
+      });
+    
+      const json = await data.json();
+      const gptMovies = json?.content[0]?.text;
+        const gptMoviesList = gptMovies.split(',')
+        // console.log(gptMoviesList);
+        const allPromises = Promise.all(gptMoviesList.map((movie) => searchMovies(movie)))
+        
+        allPromises.then((results) => dispatch(addGptMovies({gptMoviesData:results, gptResults:gptMoviesList})))
+    } catch (error) {
+      console.log(error);
+      alert('gpt api limit reached');
+    }
     
    
   }
 
   const handleClickGpt = () => {
    getDataGpt()
+   setLoader(true)
+   if(!gptResults) {
+    setLoader(false)
+   }
 
   }
 
   return (
     <div className=' pt-[30%] md:pt-[8%] flex justify-center'>
+
+<ColorRing
+  visible={loader}
+  height="80"
+  width="80"
+  ariaLabel="color-ring-loading"
+  wrapperStyle={{}}
+  wrapperClass="color-ring-wrapper"
+  colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+  />
+
     <form onSubmit={(e) => e.preventDefault()} className=' bg-black text-white w-11/12 md:w-1/2 p-4 grid grid-cols-12'>
-        <input ref={text} className='   p-2 text-gray-700 focus col-span-10 rounded-sm' type='text'   />
+        <input ref={text} placeholder='eg. action,comedy movie' className='   p-2 text-gray-700 focus col-span-10 rounded-sm' type='text'   />
         <button onClick={handleClickGpt} className=' bg-red-500 text-black py-2 ml-2 col-span-2 rounded-lg'>{languages[lang].search}</button>
     </form>
     </div>
--- src/components/SearchSuggestion.jsx
+++ src/components/SearchSuggestion.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 import MovieList from './MovieList'
 import { useSelector } from 'react-redux'
 
+
 const SearchSuggestion = () => {
   const {gptMovies,gptResults} = useSelector((store) => store.gpt)
-  if(!gptResults) return
+  if(!gptResults) return 
 
   // console.log(gptResults);
 
   return (
     <div className=' bg-black bg-opacity-80'>
-     
+       
       {gptResults.map((movieName,index) => <MovieList key={movieName} title={movieName} movies={gptMovies[index]}/>)}
     </div>
   )
